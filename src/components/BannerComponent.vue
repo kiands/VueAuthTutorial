86da0e3a80127c8ef8583361f4b07093c547772d
kiands <794583439@qq.com>
2023-05-30 22:18:25 +0800

    improved implementation of elements' css

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -6,19 +6,58 @@
         padding: 10px;
         display: flex;
         flex-direction: row;
-        align-items: center;
-        justify-content: space-between;"
+        justify-content: center;"
       >
-        <div>3473400000</div>
-        <h1 class="font-weight-bold text-h3">
-          FACE FRIENDS FOUNDATION
-        </h1>
-        <!-- 添加一个按钮来打开登录对话框 -->
-        <v-btn color="white" @click="dialog = true">
-          <v-icon>
-            mdi-account
-          </v-icon>
-        </v-btn>
+        <!--Navigation Drawer trigger button only exists on mdAndDown-->
+        <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          temporary
+        >
+          <v-list
+            nav
+            dense
+          >
+            <v-list-item-group
+              v-model="group"
+              active-class="deep-purple--text text--accent-4"
+            >
+              <v-list-item>
+                <v-list-item-title>Foo</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title>Bar</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title>Fizz</v-list-item-title>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-title>Buzz</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
+        <div style="width: 95vw; display: flex; flex-direction: column; justify-content: space-between; align-items: center">
+          <!--This will only show up on small and down screens-->
+          <div v-show="$vuetify.breakpoint.smAndDown">3473400000</div>
+          <div :class="$vuetify.breakpoint.smAndDown ? 'banner-smAndDown' : 'banner-normal'">
+            <div v-show="$vuetify.breakpoint.mdAndUp" style="display: flex; align-items: center">3473400000</div>
+            <v-btn icon @click.stop="drawer = !drawer" v-show="$vuetify.breakpoint.smAndDown">
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+            <h1 :class="$vuetify.breakpoint.smAndDown ? 'small-text' : 'normal-text'">FACE FRIENDS FOUNDATION</h1>
+            <!-- 添加一个按钮来打开登录对话框 -->
+            <v-btn icon @click="dialog = true" v-show="$vuetify.breakpoint.mdAndUp">
+              <v-icon icon>
+                mdi-account
+              </v-icon>
+            </v-btn>
+            <div v-show="$vuetify.breakpoint.smAndDown"></div>
+          </div>
+        </div>
         <!-- 登录对话框 -->
         <v-dialog v-model="dialog" max-width="500px">
           <v-card>
@@ -46,6 +85,34 @@
   </div>
 </template>
 
+<style>
+.banner-normal {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center
+}
+
+.banner-smAndDown {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center
+}
+
+.small-text {
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center
+}
+
+.normal-text {
+  font-size: 3rem;
+}
+</style>
+
 <script>
 import HelloWorld from '@/components/HelloWorld';
 import AuthComponent from '@/components/AuthComponent';
@@ -60,6 +127,7 @@ export default {
 
   data () {
     return {
+      drawer: false,
       dialog: false,
       items: [
         ['Home', '/'],
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,88 @@
 <template>
   <v-container>
     <v-row>
-      <v-col>
-        <v-card class="mx-auto" max-width="800">
-          <v-img src="@/assets/Home/img1.webp" max-width="800">
+      <v-col cols="12" sm="12" md="8">
+        <!--v-card class="mx-auto" max-width="800"-->
+        <v-card>
+          <v-img src="@/assets/Home/img1.webp">
           </v-img>
         </v-card>
       </v-col>
-      <v-col>
-        <v-card class="mx-auto" min-width="300" max-width="800" max-height="521" style="display: flex; flex-direction: column; justify-content: space-around; align-items: center;">
-          <v-card-text class="text-h5" style="margin-top: 45px; margin-bottom: 20px; display: flex; flex-direction: column; align-items: center;">
+      <v-col cols="12" sm="12" md="4" style="height: auto">
+        <!--v-card class="mx-auto" min-width="300" max-width="800" max-height="521" style="display: flex; flex-direction: column; justify-content: space-around; align-items: center;"-->
+        <v-card style="display: flex; flex-direction: column; justify-content: space-around; align-items: center;">
+          <v-card-text class="text-h5 text-center" style="padding-top: 45px; padding-bottom: 20px;">
             WELCOME
           </v-card-text>
-          <v-card-text class="text-h4 font-weight-bold" style="margin-top: -20px; padding-left: 30px; padding-right: 30px; margin-bottom: 20px; display: flex; flex-direction: column; align-items: center;">
+          <v-card-text class="text-h4 text-center font-weight-bold" style="padding-top: -20px; padding-left: 30px; padding-right: 30px; padding-bottom: 40px; display: flex; flex-direction: column; align-items: center;">
             GIVING BACK TO OUR COMMUNITY THROUGH LIFE-CHANGING SERVICES.
           </v-card-text>
           <div style="padding-bottom: 50px; display: flex; flex-direction: column; justify-content: space-between; align-items: center;">
             <v-btn style="margin-bottom: 20px">
               JOIN US
             </v-btn>
-            <v-btn>
+            <v-btn style="margin-bottom: 5px">
               LEARN MORE
             </v-btn>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!--Vuetify grid: those break points' work mode is `andUp`. Each screen size is divided in 12 cols-->
+    <v-row style="display: flex; flex-direction: row; justify-content: center">
+      <v-col cols="12" xs="12" sm="6" md="4">
+        <v-card style="display: flex; flex-direction: column; align-items: center;">
+          <v-img src="@/assets/Home/fx-gs.webp" max-width="270"></v-img>
+          <v-card-text class="text-h4 text-center">
+            OUR TEAM
+          </v-card-text>
+          <v-card-text>
+            Our amazing team of regular and part-time volunteers are committed to helping others. We take our convictions and turn them into action. Do you think you would be a good fit? Please get in touch for more information!
+          </v-card-text>
+          <v-btn style="margin-bottom: 20px;"></v-btn>
+        </v-card>
+      </v-col>
+      <v-col cols="12" xs="12" sm="6" md="4">
+        <v-card style="display: flex; flex-direction: column; align-items: center;">
+          <v-img src="@/assets/Home/fx-gs-1.webp" max-width="270"></v-img>
+          <v-card-text class="text-h4 text-center">
+            OUR HISTORY
+          </v-card-text>
+          <v-card-text>
+            Our amazing team of regular and part-time volunteers are committed to helping others. We take our convictions and turn them into action. Do you think you would be a good fit? Please get in touch for more information!
+          </v-card-text>
+          <v-btn style="margin-bottom: 20px;"></v-btn>
+        </v-card>
+      </v-col>
+      <v-col cols="12" xs="12" sm="6" md="4">
+        <v-card style="display: flex; flex-direction: column; align-items: center;">
+          <v-img src="@/assets/Home/fx-gs-2.webp" max-width="270"></v-img>
+          <v-card-text class="text-h4 text-center">
+            OUR MISSION
+          </v-card-text>
+          <v-card-text>
+            Our amazing team of regular and part-time volunteers are committed to helping others. We take our convictions and turn them into action. Do you think you would be a good fit? Please get in touch for more information!
+          </v-card-text>
+          <v-btn style="margin-bottom: 20px;"></v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" xs="12" sm="6">
+        <v-card>
+          <v-img src="@/assets/Home/img2.webp" max-width="600"></v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12" xs="12" sm="6">
+        <v-card>
+          <v-card-text class="text-h4 font-weight-bold">
+            HELP OUR CAUSE
+          </v-card-text>
+          <v-card-text class="text-h4 font-weight-bold">
+            PayPal Placeholder
+          </v-card-text>
+          <div style="display: flex; justify-content: center;">
+            <v-btn style="margin-bottom: 20px;"></v-btn>
           </div>
         </v-card>
       </v-col>

40cbc05c15ffeda7e70b1b441ea2b05308f2658a
kiands <794583439@qq.com>
2023-06-09 10:47:43 +0800

    Home layout optimization and banner modification

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container>
+    <!--Head-->
     <v-row>
       <v-col cols="12" sm="12" md="8">
         <!--v-card class="mx-auto" max-width="800"-->
-        <v-card>
-          <v-img src="@/assets/Home/img1.webp">
-          </v-img>
+        <v-card style="height: 100%; object-fit: cover">
+          <v-img src="@/assets/Home/img1.webp" style="height: 100%"></v-img>
         </v-card>
       </v-col>
       <v-col cols="12" sm="12" md="4" style="height: auto">
@@ -28,6 +28,7 @@
         </v-card>
       </v-col>
     </v-row>
+    <!--Team History Mission-->
     <!--Vuetify grid: those break points' work mode is `andUp`. Each screen size is divided in 12 cols-->
     <v-row style="display: flex; flex-direction: row; justify-content: center">
       <v-col cols="12" xs="12" sm="6" md="4">
@@ -67,22 +68,61 @@
         </v-card>
       </v-col>
     </v-row>
+    <!--Donation-->
     <v-row>
       <v-col cols="12" xs="12" sm="6">
-        <v-card>
-          <v-img src="@/assets/Home/img2.webp" max-width="600"></v-img>
+        <v-card style="height: 100%; object-fit: cover">
+          <v-img src="@/assets/Home/img2.webp" style="height: 100%"></v-img>
         </v-card>
       </v-col>
       <v-col cols="12" xs="12" sm="6">
-        <v-card>
-          <v-card-text class="text-h4 font-weight-bold">
+        <v-card style="height: 100%">
+          <v-card-text class="text-h4 font-weight-bold" style="margin-bottom: 4px">
             HELP OUR CAUSE
           </v-card-text>
-          <v-card-text class="text-h4 font-weight-bold">
-            PayPal Placeholder
+          <div style="margin-left: 20px; margin-bottom: 20px">
+            <div style="display: flex; flex-direction: column; justify-content: center;">
+              <div>
+                <v-btn style="margin-bottom: 20px;" :href="buttonLink" target="_blank">Donate</v-btn>
+              </div>
+              <v-img src="@/assets/Home/payments.png" max-width="200"></v-img>
+            </div>
+          </div>
+          <v-card-text>
+            100% of your donation will fund our mission to provide support and improve conditions for those in need. All donations are tax deductible.
           </v-card-text>
-          <div style="display: flex; justify-content: center;">
-            <v-btn style="margin-bottom: 20px;"></v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!--Fundraising flyer-->
+    <v-row>
+      <v-col>
+        <v-card col="12" xs="12" sm="12" md="12" lg="12" xl="12" style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+          <v-img src="@/assets/Home/fundraising.webp"></v-img>
+          <v-btn style="margin-bottom: 24px">BUY A DOZEN TODAY</v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
+    <!--Sponsors-->
+    <v-row>
+      <v-col>
+        <v-card style="display: flex; flex-direction: column; align-items: center">
+          <v-card-text class="text-h4 font-weight-bold" style="display: flex; flex-direction: column; align-items: center">
+            OUR SPONSORS
+          </v-card-text>
+          <div style="width: 500px; margin-bottom: 12px; display: flex; flex-direction: row; justify-content: space-between; align-items: center">
+            <div>
+              <v-img src="@/assets/Home/target.webp" max-width="50"></v-img>
+            </div>
+            <div>
+              <v-img src="@/assets/Home/hh.webp" max-width="100"></v-img>
+            </div>
+            <div>
+              <v-img src="@/assets/Home/publix.webp" max-width="100"></v-img>
+            </div>
+            <div>
+              <v-img src="@/assets/Home/sams.webp" max-width="100"></v-img>
+            </div>
           </div>
         </v-card>
       </v-col>
@@ -94,5 +134,10 @@
   export default {
     name: 'HomeView',
     // 这里是组件的JavaScript代码
+    data() {
+      return {
+        buttonLink: 'https://www.paypal.com/webapps/shoppingcart?flowlogging_id=f418588899cbc&mfid=1686273977343_f418588899cbc#/checkout/openButton'
+      }
+    }
   }
 </script>

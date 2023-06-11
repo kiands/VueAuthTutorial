0fcabc2e91d5eeeb1ef35a6c668af15e52de7b90
kiands <794583439@qq.com>
2023-06-11 21:59:21 +0800

    Optimized navigation drawer for small screens

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -1,81 +1,85 @@
 <template>
   <div>
-      <div
-        style="
-        height: 10vh;
-        padding: 10px;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;"
+    <div
+      style="
+      height: 10vh;
+      padding: 10px;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;"
+    >
+      <!--Navigation Drawer trigger button only exists on mdAndDown-->
+      <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary
       >
-        <!--Navigation Drawer trigger button only exists on mdAndDown-->
-        <v-navigation-drawer
-          v-model="drawer"
-          absolute
-          temporary
+        <v-list
+          nav
+          dense
+          v-for="item in items"
+          :key="item"
         >
-          <v-list
-            nav
-            dense
-            v-for="item in items"
-            :key="item"
-          >
-            <v-list-item @click="$router.push(links[item])">
-              <v-list-item-title>{{ item }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-        <!--This div element contains the banner text and icons-->
-        <div style="width: 95vw; display: flex; flex-direction: column; justify-content: space-between; align-items: center">
-          <!--This will only show up on small and down screens-->
-          <div v-show="$vuetify.breakpoint.smAndDown">678-765-0482 & 678-926-3808</div>
-          <div :class="$vuetify.breakpoint.smAndDown ? 'banner-smAndDown' : 'banner-normal'">
-            <div v-show="$vuetify.breakpoint.mdAndUp" style="display: flex; flex-direction: column; align-items: center">
-              <div>678-765-0482</div>
-              <div>&</div>
-              <div>678-926-3808</div>
-            </div>
-            <v-btn icon @click.stop="drawer = !drawer" v-show="$vuetify.breakpoint.smAndDown">
-              <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-            <v-spacer></v-spacer>
-            <h1 :class="$vuetify.breakpoint.smAndDown ? 'small-text' : 'normal-text'">FACE FRIENDS FOUNDATION</h1>
-            <v-spacer></v-spacer>
-            <!-- 添加一个按钮来打开登录对话框 -->
-            <v-btn icon @click="dialog = true" v-show="$vuetify.breakpoint.mdAndUp">
-              <v-icon icon>
-                mdi-account
-              </v-icon>
-            </v-btn>
-            <div v-show="$vuetify.breakpoint.smAndDown"></div>
-          </div>
+          <v-list-item @click="routeTo(links[item])">
+            <v-list-item-title>{{ item }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+        <div style="display: flex; justify-content: center">
+          <v-btn @click="dialog = true" v-if="!this.$store.state.auth.isLoggedIn">SIGN IN/UP</v-btn>
+          <v-btn @click="dialog = true" v-if="this.$store.state.auth.isLoggedIn">MANAGE</v-btn>
         </div>
-        <!-- 登录对话框 -->
-        <v-dialog v-model="dialog" max-width="500px">
-          <v-card>
-            <!-- 在这里添加你的登录组件 -->
-            <AuthComponent />
-          </v-card>
-        </v-dialog>
+      </v-navigation-drawer>
+      <!--This div element contains the banner text and icons-->
+      <div style="width: 95vw; display: flex; flex-direction: column; justify-content: space-between; align-items: center">
+        <!--This will only show up on small and down screens-->
+        <div v-show="$vuetify.breakpoint.smAndDown">678-765-0482 & 678-926-3808</div>
+        <div :class="$vuetify.breakpoint.smAndDown ? 'banner-smAndDown' : 'banner-normal'">
+          <div v-show="$vuetify.breakpoint.mdAndUp" style="display: flex; flex-direction: column; align-items: center">
+            <div>678-765-0482</div>
+            <div>&</div>
+            <div>678-926-3808</div>
+          </div>
+          <v-btn icon @click.stop="drawer = !drawer" v-show="$vuetify.breakpoint.smAndDown">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+          <v-spacer></v-spacer>
+          <h1 :class="$vuetify.breakpoint.smAndDown ? 'small-text' : 'normal-text'">FACE FRIENDS FOUNDATION</h1>
+          <v-spacer></v-spacer>
+          <!-- 添加一个按钮来打开登录对话框 -->
+          <v-btn icon @click="dialog = true" v-show="$vuetify.breakpoint.mdAndUp">
+            <v-icon icon>
+              mdi-account
+            </v-icon>
+          </v-btn>
+          <div v-show="$vuetify.breakpoint.smAndDown"></div>
+        </div>
       </div>
-      <v-card color="basil">
-        <!--Don't know why class of md-and-up does not work-->
-        <!--Use `activaTab` to bind highlight tab after each refresh-->
-        <v-tabs
-          v-model="activeTab"
-          v-show="$vuetify.breakpoint.mdAndUp"
-          background-color="transparent"
-          grow
+      <!-- 登录对话框 -->
+      <v-dialog v-model="dialog" max-width="500px">
+        <v-card>
+          <!-- 在这里添加你的登录组件 -->
+          <AuthComponent />
+        </v-card>
+      </v-dialog>
+    </div>
+    <v-card color="basil">
+      <!--Don't know why class of md-and-up does not work-->
+      <!--Use `activaTab` to bind highlight tab after each refresh-->
+      <v-tabs
+        v-model="activeTab"
+        v-show="$vuetify.breakpoint.mdAndUp"
+        background-color="transparent"
+        grow
+      >
+        <v-tab
+          v-for="item in items"
+          :key="item"
+          @click="routeTo(links[item])"
         >
-          <v-tab
-            v-for="item in items"
-            :key="item"
-            @click="routeTo(links[item])"
-          >
-            {{ item }}
-          </v-tab>
-        </v-tabs>
-      </v-card>
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+    </v-card>
   </div>
 </template>
 

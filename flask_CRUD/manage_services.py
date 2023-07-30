1a979f8487cb4c08455ef080e359daea78246a2d
kiands <794583439@qq.com>
2023-07-30 21:32:07 +0800

    Added services management backend functions.

--- flask_CRUD/manage_services.py
+++ flask_CRUD/manage_services.py
@@ -24,8 +24,8 @@ config = {
 manage_services_blueprint = Blueprint('manage_services', __name__)
 
 # 查看当前服务预订列表
-@manage_services_blueprint.route('/api/cms/services', methods=['GET'])
-def check_services():
+@manage_services_blueprint.route('/api/cms/booked_services', methods=['GET'])
+def check_booked_services():
     sql_detect = """
         select count(booking_id) from booked_services where status != -1
     """
@@ -102,3 +102,92 @@ def revoke_booking():
     cursor.close()
     conn.close()
     return jsonify("Revoked successfully!")
+
+# 查看当前提供的服务
+@manage_services_blueprint.route('/api/cms/services', methods=['GET'])
+def check_services():
+    sql_read = """
+        select * from services
+    """
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute(sql_read)
+    result = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    services = []
+    for i in range(0, len(result)):
+        services.append({
+            'service_id': result[i][0],
+            'service_name': result[i][1],
+            'service_description': result[i][2],
+            'time_slots': result[i][3]
+        })
+    return jsonify({
+        'services': services
+    })
+
+# 添加新服务
+@manage_services_blueprint.route('/api/cms/services', methods=['POST'])
+def add_new_service():
+    data = request.get_json()
+    service_name = data['service_name']
+    service_description = data['service_description']
+    sql_create = """
+        INSERT INTO services (service_name, service_description, time_slots)
+        VALUES (%s, %s, %s)
+    """
+    empty_time_slots = {}
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute(sql_create, (service_name, service_description, json.dumps(empty_time_slots)))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return "Success!"
+
+# 删除服务
+# `DELETE` seems not support payload?
+@manage_services_blueprint.route('/api/cms/services/<int:service_id>', methods=['DELETE'])
+def delete_service(service_id):
+    sql_delete = """
+        DELETE FROM services WHERE service_id = %s
+    """
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute(sql_delete, (service_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return "Success!"
+
+# 更新服务的time slots
+@manage_services_blueprint.route('/api/cms/services', methods=['PUT'])
+# @jwt_required()
+def update_service():
+    data = request.get_json()
+    service_name = data['service_name']
+    # New time slots.
+    new_time_slots = data['new_time_slots']
+    sql_read = """
+        select time_slots from services
+        where service_name = %s
+    """
+    sql_update = """
+        UPDATE services
+        SET time_slots = %s
+        WHERE service_name = %s
+    """
+
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+    cursor.execute(sql_read, (service_name,))
+    result = cursor.fetchall()
+    slots = json.loads(result[0][0])
+    # Dict has 'update()'.
+    slots.update(new_time_slots)
+    cursor.execute(sql_update, (json.dumps(slots), service_name))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return "Success!"
